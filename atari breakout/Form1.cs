b658264ebce84c71eac0b340023b0d2c4a22a190
UrosSysPro <71965086+UrosSysPro@users.noreply.github.com>
2022-05-22 16:59:14 +0200

    Add files via upload

---
+++ atari breakout/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace atari_breakout
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        Graphics g;
+        SolidBrush brush;
+        Pen pen;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            g = CreateGraphics();
+            brush = new SolidBrush(Color.Black);
+            pen = new Pen(brush);  
+        }
+
+        
+    }
+}
