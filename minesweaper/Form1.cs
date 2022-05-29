0b5df4685bae9f3ce82b37cf3bb10c6acdec7623
UrosSysPro <71965086+UrosSysPro@users.noreply.github.com>
2022-05-29 13:23:03 +0200

    Add files via upload

--- minesweaper/Form1.cs
+++ minesweaper/Form1.cs
@@ -25,15 +25,15 @@ namespace minesweeper
         private void Form1_Load(object sender, EventArgs e)
         {
             mine = new bool[8, 8];
-            for(int i = 0; i < 8; i++)
+            for (int i = 0; i < 8; i++)
             {
-                for(int j = 0; j < 8; j++)
+                for (int j = 0; j < 8; j++)
                 {
                     mine[i, j] = false;
                 }
             }
 
-            for(int i = 0; i < 7; i++)
+            for (int i = 0; i < 7; i++)
             {
                 int x = r.Next(8);
                 int y = r.Next(8);
@@ -53,10 +53,10 @@ namespace minesweeper
                         labels[i, j].BackColor = Color.DimGray;
                     else
                         labels[i, j].BackColor = Color.Wheat;
-                        
-                     
-                    labels[i, j].Font= new Font("Arial", 20);
-                    labels[i, j].Location = new Point(i * velicina+10, j * velicina+10);
+
+
+                    labels[i, j].Font = new Font("Arial", 20);
+                    labels[i, j].Location = new Point(i * velicina + 10, j * velicina + 10);
                     Controls.Add(labels[i, j]);
                 }
             }
@@ -66,30 +66,30 @@ namespace minesweeper
             {
                 for (int j = 0; j < 8; j++)
                 {
-                    pbs[i,j] = new PictureBox();
+                    pbs[i, j] = new PictureBox();
                     pbs[i, j].Size = new Size(velicina, velicina);
                     pbs[i, j].Location = new Point(i * velicina, j * velicina);
-                    if (i % 2 ==  j % 2)
+                    if (i % 2 == j % 2)
                         pbs[i, j].BackColor = Color.DimGray;
                     else
                         pbs[i, j].BackColor = Color.Wheat;
 
-                    pbs[i, j].Click += (o,args)=>{
+                    pbs[i, j].Click += (o, args) => {
                         klik(o);
                     };
                     Controls.Add(pbs[i, j]);
                 }
             }
 
-            
+
         }
 
 
         public void klik(Object o)
         {
-            int x=0, y=0;
-            for(int i=0;i<8;i++)
-                for(int j=0;j<8;j++)
+            int x = 0, y = 0;
+            for (int i = 0; i < 8; i++)
+                for (int j = 0; j < 8; j++)
                     if (pbs[i, j] == o)
                     {
                         x = i; y = j;
@@ -110,14 +110,14 @@ namespace minesweeper
                 int brojMina = 0;
                 for (int i = -1; i <= 1; i++)
                     for (int j = -1; j <= 1; j++)
-                        if(
+                        if (
                             x + i >= 0 &&
-                            x + i <  8 &&
+                            x + i < 8 &&
                             y + j >= 0 &&
-                            y + j <  8)
-                        if (mine[x + i,y + j])
-                            brojMina++;
-                labels[x, y].Text =""+ brojMina;
+                            y + j < 8)
+                            if (mine[x + i, y + j])
+                                brojMina++;
+                labels[x, y].Text = "" + brojMina;
                 labels[x, y].Visible = true;
             }
         }
